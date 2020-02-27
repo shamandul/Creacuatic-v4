142902080102a1b2df23d1ba76bc84b7a37f6e01
Jesus SP <shamandul@gmail.com>
2020-02-27 21:03:26 +0100

    creacion de formulario y añadir redes sociales

--- src/components/FormularioContacto.js
+++ src/components/FormularioContacto.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react'
+import img from '../images/undraw_contact_us_15o2.svg'
 
 class FormularioContacto extends Component{
     render(){
         return(
             <React.Fragment>
-                <form>
-                    <div class="form-group">
-                      <label for=""></label>
-                      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId"/>
-                      <small id="helpId" class="text-muted">Help text</small>
+              <div className="col-md-4 mx-auto mt-4 mb-4">
+                <div className="card text-left">
+                  .<div className="card">
+                    <div className="card-header">
+                    <img className="card-img-top" src={img} alt=""/>
                     </div>
-                    <div class="form-group">
-                      <label for=""></label>
-                      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId"/>
-                      <small id="helpId" class="text-muted">Help text</small>
-                    </div>
-                    <div class="form-group">
-                      <label for=""></label>
-                      <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId"/>
-                      <small id="helpId" class="text-muted">Help text</small>
-                    </div>
-                </form>
+                    <div className="card-body">
+                      <h4 className="card-title">Contacto</h4>
+                      <form method="POST">
+                        <div className="form-group">
+                          <input type="text" name="nombre" className="form-control" placeholder="Introduce tu nombre" required/>
+                        </div>
+                        <div className="form-group">
+                          <input type="email" name="email" className="form-control" placeholder="ejemplo@ejemplo.com" required/>
+                        </div>
+                        <div className="form-group">
+                          <input type="tel" name="telefono" className="form-control" placeholder="Teléfono"/>
+                        </div>
+                        <div className="form-group">
+                          <textarea name="texto" className="form-control"/>
+                        </div>
+                        <div className="card-footer">
+                          <button type="submit" className="form-control btn btn-success" >Enviar</button>
+                        </div>
+                      </form>
+                    </div> 
+                  </div>
+                </div>
+              </div>
+            
+          
+                
             </React.Fragment>
         )
     }
--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import Navegador from '../components/Navegador'
 import Cabecera from '../components/Cabecera'
 import imagen from '../images/coral.jpg'
-import formulario from '../components/FormularioContacto'
 import FormularioContacto from '../components/FormularioContacto'
+import Footer from '../components/Footer'
 
 class Contacto extends Component{
     render(){
@@ -15,7 +15,8 @@ class Contacto extends Component{
                     subtitle="Contacto"
                     img = {imagen}
                 />
-                <FormularioContacto></FormularioContacto>
+                <FormularioContacto/>
+                <Footer/>
             </React.Fragment>
         )
     }
