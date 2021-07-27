09b297cbdb8567fa2df92421ee1fb183564e3542
Jesus SP <shamandul@gmail.com>
2021-07-27 20:32:36 +0200

    actualización

--- src/components/UltimosList.js
+++ src/components/UltimosList.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import Elemento from './Elemento'
+import React from "react";
+import Elemento from "./Elemento";
 
-function UltimosList(props){
-    return(
-        <React.Fragment>
-            { props.elementos.map((elemento)=> {
-                return(
-                    <Elemento
-                        id ={elemento.id}
-                        key={elemento.id}
-                        titulo={elemento.titulo}
-                        descripcion={elemento.descripcion}
-                        numero={elemento.numero}
-                        estado={elemento.estado}
-                     />
-                )
-                 
-                })
-            }
-        </React.Fragment>
-    )
+function UltimosList(props) {
+  return (
+    <React.Fragment>
+      {props.elementos.map((elemento) => {
+        return (
+          <Elemento
+            id={elemento.id}
+            key={elemento.id}
+            titulo={elemento.titulo}
+            descripcion={elemento.descripcion}
+            numero={elemento.numero}
+            estado={elemento.estado}
+          />
+        );
+      })}
+    </React.Fragment>
+  );
 }
-export default UltimosList
+export default UltimosList;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react'
-import Navegador from '../components/Navegador'
-import Cabecera from '../components/Cabecera'
-import Description from '../components/Description'
-import Footer from '../components/Footer'
-import imagen from '../images/pez-sierra.jpg'
-import './Styles/servicios.css'
+import React, { Component } from "react";
+import Navegador from "../components/Navegador";
+import Cabecera from "../components/Cabecera";
+import Description from "../components/Description";
+import Footer from "../components/Footer";
+import imagen from "../images/pez-sierra.jpg";
+import "./Styles/servicios.css";
 
-class About extends Component{
-    render(){
-        return(
-            <React.Fragment>
-                <Navegador/>
-                <Cabecera
-                    title="Creacuatic"
-                    subtitle="Sobre nosotros"
-                    img = {imagen}
-                />
-                <Description
-                    title="Sobre nosotros"
-                    descripcion="Somos un equipo multidisciplinar de profesionales apasionados por
+class About extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navegador />
+        <Cabecera title="Creacuatic" subtitle="Sobre nosotros" img={imagen} />
+        <Description
+          title="Sobre nosotros"
+          descripcion="Somos un equipo multidisciplinar de profesionales apasionados por
                      los ecosistemas acuáticos, trabajando para dar el mejor asesoramiento y formación online."
-                >
-                </Description>
-                
-                <Footer/>         
-            </React.Fragment>
-        )
-    }
+        ></Description>
+
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
-export default About
+export default About;
